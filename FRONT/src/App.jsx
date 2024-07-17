fbd32eabddf3253d3916fc2cd3dbac9342a060a6
김일태 <iltae94@gmail.com>
2024-07-17 16:08:00 +0900

    :wrench: Config: CSS THEME 설정

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -1,5 +1,32 @@
+import React from "react";
+
+import Button from "@mui/material/Button";
+
+import { useTheme } from "@mui/material";
+
 function App() {
-  return <></>;
+  const theme = useTheme();
+  return (
+    <>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: theme.palette.primary.main,
+          fontSize: theme.fontSize.basic,
+        }}
+      >
+        Hello world
+      </Button>
+      <span
+        style={{
+          fontWeight: theme.typography.fontWeight,
+          fontSize: theme.typography.fontSize,
+        }}
+      >
+        하이
+      </span>
+    </>
+  );
 }
 
 export default App;
