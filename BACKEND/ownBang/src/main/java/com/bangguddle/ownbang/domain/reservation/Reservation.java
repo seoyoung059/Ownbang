54de677bcb08c1e828db9ff314c34b4742c9ae4e
오유진 <oyj5775@example.com>
2024-07-18 16:38:29 +0900

    ✨ Feat: 예약DTO 추가

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/reservation/Reservation.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/reservation/Reservation.java
@@ -2,11 +2,12 @@ package com.bangguddle.ownbang.domain.reservation;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
+@Builder
 @Entity
 @Table(name = "reservation")
 @Getter
---
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/reservation/ReservationDto.java
@@ -0,0 +1,38 @@
+package com.bangguddle.ownbang.domain.reservation;
+
+import java.time.LocalDateTime;
+
+public record ReservationDto(
+        Long id,
+        Long roomId,
+        Long userId,
+        LocalDateTime time,
+        ReservationStatus status
+
+) {
+
+    public static ReservationDto of( Long id, Long roomId, Long userId, LocalDateTime time, ReservationStatus status) {
+        return new ReservationDto(id, roomId, userId, time, status);
+    }
+
+    public static ReservationDto from(Reservation reservation){
+        return new ReservationDto(
+                reservation.getId(),
+                reservation.getRoomId(),
+                reservation.getUserId(),
+                reservation.getTime(),
+                reservation.getStatus()
+        );
+    }
+
+
+    public Reservation toEntity() {
+        return Reservation.builder()
+                .id(id)
+                .roomId(roomId)
+                .userId(userId)
+                .time(time)
+                .status(status)
+                .build();
+    }
+}
