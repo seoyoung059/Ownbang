3e7e92797a2352ef50d0242964f4323300661c1b
김일태 <iltae94@gmail.com>
2024-07-18 16:12:31 +0900

    [Feat] 전역 저장소 Zustand 기초 설정

--- FRONT/src/api/index.js
+++ FRONT/src/api/index.js
@@ -1,1 +1,13 @@
-// 여기는 axios를 구조화할 거에용
+import axios from "axios";
+
+// axios 요청 구조화
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  timeout: 10000, // 요청 타임아웃 설정
+  headers: {
+    "Content-Type": "application/json",
+    // 기타 필요한 헤더 설정
+  },
+});
+
+export default axiosInstance;
