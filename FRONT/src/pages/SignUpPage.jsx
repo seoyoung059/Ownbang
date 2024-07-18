ce64ffed5adbf1119d7054d08a9eec658dce7b05
dlthgml1501 <dlthgml1501@gmail.com>
2024-07-19 08:55:41 +0900

    � Design: 회원가입 페이지 CSS 코드 통일, 제목과 폼 분리

--- FRONT/src/pages/SignUpPage.jsx
+++ FRONT/src/pages/SignUpPage.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useTheme } from "@mui/material/styles";
+import SignUpForm from "../components/sign-up/SignUpForm";
 
 export default function SignUpPage() {
-  return <div>SingUpPage</div>;
+  const theme = useTheme();
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          component="h1"
+          variant="h5"
+          sx={{ fontWeight: theme.fontWeight.bold, mt: 8 }}
+        >
+          회원가입
+        </Typography>
+        <SignUpForm />
+      </Box>
+    </Container>
+  );
 }
