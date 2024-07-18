f65dc6f0eae825c492a53433bd1af9ed73784c8d
seoyoung059 <popolley@gmail.com>
2024-07-19 00:00:05 +0900

    ✨ Feat: RoomImage Entity 생성 - #S11P12A702-200 완료

---
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/entity/RoomImage.java
@@ -0,0 +1,25 @@
+package com.bangguddle.ownbang.domain.room.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "room_image")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RoomImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="room_image_id", nullable = false, columnDefinition = "UNSIGNED")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="room_id", nullable = false, columnDefinition = "UNSIGNED")
+    private Room room;
+
+    @Column(name="room_image_url", nullable = false, length = 255)
+    private String roomImageUrl;
+}
