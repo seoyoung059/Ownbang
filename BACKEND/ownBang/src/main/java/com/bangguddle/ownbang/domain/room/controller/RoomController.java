8ffc4bcb26f86d53944f252e8023be84c24c187b
seoyoung059 <popolley@gmail.com>
2024-07-19 09:56:56 +0900

    ✨ Feat: Room 관련 repo, controller, service 생성

---
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/controller/RoomController.java
@@ -0,0 +1,25 @@
+package com.bangguddle.ownbang.domain.room.controller;
+
+import com.bangguddle.ownbang.domain.room.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/rooms")
+@RequiredArgsConstructor
+public class RoomController {
+
+    private final RoomService roomService;
+
+    // 매물 등록
+
+
+    // 매물 수정
+
+    // 매물 삭제
+
+    // 매물 단건 조회
+
+}
---
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/service/RoomService.java
@@ -0,0 +1,9 @@
+package com.bangguddle.ownbang.domain.room.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService {
+
+
+}
