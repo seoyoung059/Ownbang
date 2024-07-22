ad59e448ce5dbfff2926e4a8958f5f6058d4236c
서정희 <sjhlko@naver.com>
2024-07-22 12:42:55 +0900

    🐛 Fix: 서비스에서 리턴한 Response 객체를 그대로 리턴할 수 있도록 공통 Response 로직 수정 - #S11P12A702-267 완료

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/response/SuccessResponse.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/response/SuccessResponse.java
@@ -2,5 +2,5 @@ package com.bangguddle.ownbang.global.response;
 
 import com.bangguddle.ownbang.global.enums.SuccessCode;
 
-public record SuccessResponse<T>(SuccessCode successCode, T result) {
+public record SuccessResponse<T>(SuccessCode successCode, T data) {
 }
