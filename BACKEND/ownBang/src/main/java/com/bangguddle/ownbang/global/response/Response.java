9332a576b6ca936ec97aaea51e1c67bf46fca8ac
서정희 <sjhlko@naver.com>
2024-07-22 12:42:55 +0900

    🐛 Fix: 서비스에서 리턴한 Response 객체를 그대로 리턴할 수 있도록 공통 Response 로직 수정 - #S11P12A702-267 완료

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/response/Response.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/response/Response.java
@@ -1,14 +1,13 @@
 package com.bangguddle.ownbang.global.response;
 
 import com.bangguddle.ownbang.global.enums.ErrorCode;
-import com.bangguddle.ownbang.global.enums.SuccessCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public record Response<T>(String resultCode, String code, String message, T data) {
-    public static <T> ResponseEntity<Response<T>> success(SuccessCode code, T data) {
-        return new ResponseEntity<>(new Response<>("SUCCESS", code.name(),
-                code.getMessage(), data), HttpStatus.OK);
+    public static <T> ResponseEntity<Response<T>> success(SuccessResponse<T> response) {
+        return new ResponseEntity<>(new Response<>("SUCCESS", response.successCode().name(),
+                response.successCode().getMessage(), response.data()), HttpStatus.OK);
     }
 
     public static <T> Response<T> error(ErrorCode code, T data) {
