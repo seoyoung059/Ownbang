d6bea6a4cc9f0abc5e8e275d17dfafe95188522f
김서영 <popolley@gmail.com>
2024-07-23 17:21:21 +0900

    ✨ Feat: 매물 등록 API 컨트롤러, 서비스 작성 완료 - S11P12A702-277, S11P12A702-278

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/service/impl/RoomImageServiceImpl.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/service/impl/RoomImageServiceImpl.java
@@ -4,8 +4,10 @@ package com.bangguddle.ownbang.domain.room.service.impl;
 import com.bangguddle.ownbang.domain.room.entity.Room;
 import com.bangguddle.ownbang.domain.room.entity.RoomImage;
 import com.bangguddle.ownbang.domain.room.repository.RoomImageRepository;
+import com.bangguddle.ownbang.global.enums.ErrorCode;
 import com.bangguddle.ownbang.global.enums.NoneResponse;
 import com.bangguddle.ownbang.global.enums.SuccessCode;
+import com.bangguddle.ownbang.global.handler.AppException;
 import com.bangguddle.ownbang.global.response.SuccessResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -25,7 +27,7 @@ public class RoomImageServiceImpl {
     private final RoomImageRepository roomImageRepository;
 
     @Transactional
-    protected SuccessResponse<NoneResponse> uploadImage(MultipartFile roomImage, Room room) throws IOException{
+    public SuccessResponse<NoneResponse> uploadImage(MultipartFile roomImage, Room room) {
         //파일명: UUID + 사진 원래이름
         String fileName = UUID.randomUUID().toString().replace("-", "") + "_" + roomImage.getOriginalFilename();
 
@@ -39,9 +41,7 @@ public class RoomImageServiceImpl {
             }
             Files.write(filePath, roomImage.getBytes());
         } catch (IOException e) {
-            throw new IOException("파일 저장 실패! - "+filePath);
-        } catch (Exception e){
-            throw e;
+            throw new AppException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
         room.getRoomImages().add(new RoomImage(room, dbFilePath));
 
--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/enums/SuccessCode.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/enums/SuccessCode.java
@@ -8,7 +8,21 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum SuccessCode implements ResponseCode {
     // Auth API
-    SIGNUP_SUCCESS(HttpStatus.CREATED, "회원가입이 성공적으로 완료되었습니다.");
+    SIGNUP_SUCCESS(HttpStatus.CREATED, "회원가입이 성공적으로 완료되었습니다."),
+
+
+
+    // Room API
+    ROOM_REGISTER_SUCCESS(HttpStatus.CREATED, "매물이 성공적으로 등록되었습니다."),
+    ROOM_UPDATE_SUCCESS(HttpStatus.OK, "매물 정보가 성공적으로 수정되었습니다."),
+    ROOM_DELETE_SUCCESS(HttpStatus.OK, "매물이 성공적으로 삭제되었습니다."),
+
+
+
+    ROOM_IMAGE_UPLOAD_SUCCESS(HttpStatus.OK, "사진이 성공적으로 저장되었습니다.");
+
+
+
 
     private final HttpStatus httpStatus;
     private final String message;
