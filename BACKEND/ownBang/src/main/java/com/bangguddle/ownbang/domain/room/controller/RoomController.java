b66ea5d491fb70dec123b4a01fbd0b1771dbb3c7
김서영 <popolley@gmail.com>
2024-07-23 10:02:21 +0900

    ♻️ Refactor: @RestController로 수정

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/controller/RoomController.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/controller/RoomController.java
@@ -1,10 +1,10 @@
 package com.bangguddle.ownbang.domain.room.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("api/rooms")
 @RequiredArgsConstructor
 public class RoomController {
