ab7dd8a84f8a11c138312977a5aa60f9c4234171
김서영 <popolley@gmail.com>
2024-07-23 23:19:12 +0900

    ♻️ Refactor: 유효하지 않은 이미지 파일일 경우의 ErrorCode 추가

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/enums/ErrorCode.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/enums/ErrorCode.java
@@ -11,6 +11,9 @@ public enum ErrorCode implements ResponseCode {
     EMAIL_DUPLICATED(HttpStatus.CONFLICT, "이미 가입한 이메일입니다."),
     PHONE_NUMBER_DUPLICATED(HttpStatus.CONFLICT, "이미 가입한 전화번호 입니다."),
 
+    // Room API
+    INVALID_IMAGE_FILE(HttpStatus.BAD_REQUEST, "유효하지 않은 이미지 파일입니다."),
+
     // Common Error Code
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부적 에러가 발생했습니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "유효하지 않은 요청입니다."),
