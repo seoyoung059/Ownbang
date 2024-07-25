c3e192de91a5b78d29a914c1b2d5c5cf90b40d11
김서영 <popolley@gmail.com>
2024-07-25 12:17:29 +0900

    ✅ Test: roomDelete 서비스 테스트 작성 - S11P12A702-290

--- BACKEND/ownBang/src/test/java/com/bangguddle/ownbang/domain/room/service/impl/RoomServiceImplTest.java
+++ BACKEND/ownBang/src/test/java/com/bangguddle/ownbang/domain/room/service/impl/RoomServiceImplTest.java
@@ -56,15 +56,16 @@ class RoomServiceImplTest {
     void createRoomTest_SUCCESS() throws ParseException {
         //given
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        RoomAppliancesCreateRequest roomAppliancesCreateRequest = new RoomAppliancesCreateRequest(true,
-                true,true,true,true,true,true,true);
-        RoomDetailCreateRequest roomDetailCreateRequest = new RoomDetailCreateRequest((byte) 1,(byte) 1,
-                HeatingType.지역, sdf.parse("2024-08-22"),7L,true, 10L,0.66f,
-                sdf.parse("2020-04-11"), sdf.parse("2020-07-01"), Facing.남,Purpose.공동주택,
+        RoomAppliancesCreateRequest roomAppliancesCreateRequest = RoomAppliancesCreateRequest.of(true,
+                true, true, true, true, true, true, true);
+        RoomDetailCreateRequest roomDetailCreateRequest = RoomDetailCreateRequest.of((byte) 1, (byte) 1,
+                HeatingType.지역, sdf.parse("2024-08-22"), 7L, true, 10L, 0.66f,
+                sdf.parse("2020-04-11"), sdf.parse("2020-07-01"), Facing.남, Purpose.공동주택,
                 "서울시 강남구 역삼대로", "멀티캠퍼스 역삼");
-        RoomCreateRequest roomCreateRequest = new RoomCreateRequest(DealType.월세, RoomType.오피스텔, Structure.분리형,
-                true, 12.88f, 15.66f,(byte)1, 3000L, 10L, 10L,
-                "parcel", "url",roomAppliancesCreateRequest, roomDetailCreateRequest);
+        RoomCreateRequest roomCreateRequest = RoomCreateRequest.of(DealType.월세, RoomType.오피스텔, Structure.분리형,
+                true, 12.88f, 15.66f, (byte) 1, 3000L, 10L, 10L,
+                "parcel", "url", roomAppliancesCreateRequest, roomDetailCreateRequest);
+
         List<MultipartFile> roomImageFiles = new ArrayList<>();
         roomImageFiles.add(new MockMultipartFile("file", "image1.png", "image/png", "image/png".getBytes()));
         roomImageFiles.add(new MockMultipartFile("file", "image2.png", "image/png", "image/png".getBytes()));
@@ -93,15 +94,15 @@ class RoomServiceImplTest {
     void createRoomTest_ImageFailed() throws IOException, ParseException {
         //given
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        RoomAppliancesCreateRequest roomAppliancesCreateRequest = new RoomAppliancesCreateRequest(true,
-                true,true,true,true,true,true,true);
-        RoomDetailCreateRequest roomDetailCreateRequest = new RoomDetailCreateRequest((byte) 1,(byte) 1,
-                HeatingType.지역, sdf.parse("2024-08-22"),7L,true, 10L,0.66f,
-                sdf.parse("2020-04-11"), sdf.parse("2020-07-01"), Facing.남,Purpose.공동주택,
+        RoomAppliancesCreateRequest roomAppliancesCreateRequest = RoomAppliancesCreateRequest.of(true,
+                true, true, true, true, true, true, true);
+        RoomDetailCreateRequest roomDetailCreateRequest = RoomDetailCreateRequest.of((byte) 1, (byte) 1,
+                HeatingType.지역, sdf.parse("2024-08-22"), 7L, true, 10L, 0.66f,
+                sdf.parse("2020-04-11"), sdf.parse("2020-07-01"), Facing.남, Purpose.공동주택,
                 "서울시 강남구 역삼대로", "멀티캠퍼스 역삼");
-        RoomCreateRequest roomCreateRequest = new RoomCreateRequest(DealType.월세, RoomType.오피스텔, Structure.분리형,
-                true, 12.88f, 15.66f,(byte)1, 3000L, 10L, 10L,
-                "parcel", "url",roomAppliancesCreateRequest, roomDetailCreateRequest);
+        RoomCreateRequest roomCreateRequest = RoomCreateRequest.of(DealType.월세, RoomType.오피스텔, Structure.분리형,
+                true, 12.88f, 15.66f, (byte) 1, 3000L, 10L, 10L,
+                "parcel", "url", roomAppliancesCreateRequest, roomDetailCreateRequest);
         List<MultipartFile> roomImageFiles = new ArrayList<>();
         roomImageFiles.add(new MockMultipartFile("file", "image1.png", "image/png", "image/png".getBytes()));
         roomImageFiles.add(new MockMultipartFile("file", "image2.png", "image/png", "image/png".getBytes()));
@@ -109,14 +110,45 @@ class RoomServiceImplTest {
         // when
         when(roomImageServiceImpl.uploadImage(any(MultipartFile.class), any(Room.class))).thenThrow(new AppException(ErrorCode.INTERNAL_SERVER_ERROR));
 
-        // 매물 생성
-        assertThatThrownBy(()->{
+        assertThatThrownBy(() -> {
             roomServiceImpl.createRoom(roomCreateRequest, roomImageFiles);
         }).isInstanceOf(AppException.class);
 
 
         verify(roomRepository, never()).save(any(Room.class));
 
+    }
+
+
+    @Test
+    @DisplayName("매물 삭제 성공")
+    void deleteRoomTest_Success() throws ParseException {
+        //given
+        when(roomRepository.existsById(anyLong())).thenReturn(true);
+        Long roomId = 1L;
+
+        //when
+        SuccessResponse<NoneResponse> response = roomServiceImpl.deleteRoom(roomId);
+
+        // Then
+        assertThat(response).isNotNull();
+        assertThat(response.successCode()).isEqualTo(SuccessCode.ROOM_DELETE_SUCCESS);
+        assertThat(response.data()).isEqualTo(NoneResponse.NONE);
+
+        verify(roomRepository, times(1)).deleteById(roomId);
+    }
+
+    @Test
+    @DisplayName("매물 삭제 실패 - 존재하지 않는 ID")
+    void deleteRoomTest_Fail_NotExist() {
+        //given
+        when(roomRepository.existsById(anyLong())).thenReturn(false);
+        Long roomId = 1L;
+
+        //when, then
+        assertThatThrownBy(() -> {
+            roomServiceImpl.deleteRoom(roomId);
+        }).isInstanceOf(AppException.class);
     }
 
 
