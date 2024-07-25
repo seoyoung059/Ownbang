1c73ec6674fd1fa270467e941b8d5f0e2ca3af9a
김서영 <popolley@gmail.com>
2024-07-25 17:48:57 +0900

    ✨ Feat: 매물 단건 조회 서비스 개발 - S11P12A702-301

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/service/impl/RoomServiceImpl.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/service/impl/RoomServiceImpl.java
@@ -1,16 +1,16 @@
 package com.bangguddle.ownbang.domain.room.service.impl;
 
 import com.bangguddle.ownbang.domain.room.dto.RoomCreateRequest;
+import com.bangguddle.ownbang.domain.room.dto.RoomSelectResponse;
 import com.bangguddle.ownbang.domain.room.entity.Room;
 import com.bangguddle.ownbang.domain.room.entity.RoomAppliances;
 import com.bangguddle.ownbang.domain.room.entity.RoomDetail;
-import com.bangguddle.ownbang.domain.room.repository.RoomAppliancesRepository;
-import com.bangguddle.ownbang.domain.room.repository.RoomDetailRepository;
-import com.bangguddle.ownbang.domain.room.repository.RoomImageRepository;
 import com.bangguddle.ownbang.domain.room.repository.RoomRepository;
 import com.bangguddle.ownbang.domain.room.service.RoomService;
+import com.bangguddle.ownbang.global.enums.ErrorCode;
 import com.bangguddle.ownbang.global.enums.NoneResponse;
 import com.bangguddle.ownbang.global.enums.SuccessCode;
+import com.bangguddle.ownbang.global.handler.AppException;
 import com.bangguddle.ownbang.global.response.SuccessResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,16 +26,12 @@ public class RoomServiceImpl implements RoomService {
 
     private final RoomRepository roomRepository;
     private final RoomImageServiceImpl roomImageServiceImpl;
-    private final RoomDetailRepository roomDetailRepository;
-    private final RoomAppliancesRepository roomAppliancesRepository;
-    private final RoomImageRepository roomImageRepository;
-//    private final UserRepository userRepository;
 
     // 매물 생성
     @Override
     @Transactional
-    public SuccessResponse<NoneResponse> createRoom(RoomCreateRequest roomCreateRequest, List<MultipartFile> roomImageFiles) {
-//        User agent = userRepository(findById(roomCreateRequest.getAgent())).orElseThrow(() -> new IllegalAccessException("Invalid Agent Id"));
+    public SuccessResponse<NoneResponse> createRoom(RoomCreateRequest roomCreateRequest, List<MultipartFile> roomImageFiles)  {
+//        User agent = userRepository(findById(roomCreateRequestDto.getAgent())).orElseThrow(() -> new IllegalAccessException("Invalid Agent Id"));
 
         RoomDetail roomDetail = roomCreateRequest.roomDetailCreateRequest().toEntity();
         RoomAppliances roomAppliances = roomCreateRequest.roomAppliancesCreateRequest().toEntity();
@@ -61,4 +58,15 @@ public class RoomServiceImpl implements RoomService {
     }
 
 
+    // 매물 조회
+    @Override
+    @Transactional
+    public SuccessResponse<RoomSelectResponse> getRoom(Long id) {
+        Optional<Room> room = roomRepository.findById(id);
+        if (room.isEmpty()) {
+            throw new AppException(ErrorCode.ROOM_NOT_FOUND);
+        }
+
+        return new SuccessResponse<>(SuccessCode.ROOM_FIND_SUCCESS, RoomSelectResponse.from(room.get()));
+    }
 }
