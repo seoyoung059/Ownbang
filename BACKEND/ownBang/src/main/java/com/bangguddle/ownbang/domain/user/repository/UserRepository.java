f49bc325b70647185d0040e98c82fda747355cab
서정희 <sjhlko@naver.com>
2024-07-25 16:33:01 +0900

    ♻️ Refactor: validation 코드를 service 로 이동

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/user/repository/UserRepository.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/user/repository/UserRepository.java
@@ -1,27 +1,14 @@
 package com.bangguddle.ownbang.domain.user.repository;
 
 import com.bangguddle.ownbang.domain.user.entity.User;
-import com.bangguddle.ownbang.global.handler.AppException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
-import static com.bangguddle.ownbang.global.enums.ErrorCode.*;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(final String email);
 
     Optional<User> findByPhoneNumber(final String phoneNumber);
-
-    default void validateByEmail(final String email) {
-        if (findByEmail(email).isPresent())
-            throw new AppException(EMAIL_DUPLICATED);
-    }
-
-    default void validateByPhoneNumber(final String phoneNumber) {
-        if (findByPhoneNumber(phoneNumber).isPresent())
-            throw new AppException(PHONE_NUMBER_DUPLICATED);
-    }
 }
