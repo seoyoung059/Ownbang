d984a6dace533bb80d95ba26b88c3a54de2a53a5
oyj5775 <oyj5775@naver.com>
2024-07-26 14:38:05 +0900

    ✨ Feat: 중개인 예약 확정 API Test 코드 추가

--- BACKEND/ownBang/src/test/java/com/bangguddle/ownbang/domain/reservation/controller/AgentReservationControllerTest.java
+++ BACKEND/ownBang/src/test/java/com/bangguddle/ownbang/domain/reservation/controller/AgentReservationControllerTest.java
@@ -1,4 +1,56 @@
 package com.bangguddle.ownbang.domain.reservation.controller;
 
+import com.bangguddle.ownbang.domain.reservation.service.ReservationService;
+import com.bangguddle.ownbang.global.enums.NoneResponse;
+import com.bangguddle.ownbang.global.enums.SuccessCode;
+import com.bangguddle.ownbang.global.response.SuccessResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AgentReservationController.class)
 public class AgentReservationControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ReservationService reservationService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("예약 확정 성공")
+    @WithMockUser
+    void confirmStatusReservation_Success() throws Exception {
+        Long id = 1L;
+
+        SuccessResponse<NoneResponse> successResponse = new SuccessResponse<>(
+                SuccessCode.RESERVATION_CONFIRM_SUCCESS,
+                NoneResponse.NONE
+        );
+
+        when(reservationService.confirmStatusReservation(anyLong())).thenReturn(successResponse);
+
+        mockMvc.perform(patch("/api/agents/reservations/{id}", id)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("SUCCESS"))
+                .andExpect(jsonPath("$.code").value(SuccessCode.RESERVATION_CONFIRM_SUCCESS.name()))
+                .andExpect(jsonPath("$.message").value(SuccessCode.RESERVATION_CONFIRM_SUCCESS.getMessage()))
+                .andExpect(jsonPath("$.data").value("NONE"));
+    }
+
 }
