ce19716600aa3de4185faa5bc92409f14a836a3e
김서영 <popolley@gmail.com>
2024-07-30 20:48:29 +0900

    ✨ Feat: 이미지를 S3에 업로드하기 위한 서비스 메서드 작성 -S11P12A702-340

---
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/service/RoomImageService.java
@@ -0,0 +1,16 @@
+package com.bangguddle.ownbang.domain.room.service;
+
+import com.bangguddle.ownbang.domain.room.entity.Room;
+import com.bangguddle.ownbang.global.enums.NoneResponse;
+import com.bangguddle.ownbang.global.handler.AppException;
+import com.bangguddle.ownbang.global.response.SuccessResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface RoomImageService {
+    SuccessResponse<NoneResponse> uploadImage(MultipartFile roomImage, Room room) throws AppException;
+
+    SuccessResponse<NoneResponse> uploadImageToS3(MultipartFile roomImage, Room room) throws AppException;
+
+    SuccessResponse<NoneResponse> deleteImage(Long roomImageId) throws AppException;
+
+}
--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/service/impl/RoomImageServiceImpl.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/service/impl/RoomImageServiceImpl.java
@@ -4,9 +4,11 @@ package com.bangguddle.ownbang.domain.room.service.impl;
 import com.bangguddle.ownbang.domain.room.entity.Room;
 import com.bangguddle.ownbang.domain.room.entity.RoomImage;
 import com.bangguddle.ownbang.domain.room.repository.RoomImageRepository;
+import com.bangguddle.ownbang.domain.room.service.RoomImageService;
 import com.bangguddle.ownbang.global.enums.NoneResponse;
 import com.bangguddle.ownbang.global.handler.AppException;
 import com.bangguddle.ownbang.global.response.SuccessResponse;
+import com.bangguddle.ownbang.global.service.S3UploaderService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
 
 import static com.bangguddle.ownbang.global.enums.ErrorCode.*;
@@ -24,17 +27,19 @@ import static com.bangguddle.ownbang.global.enums.SuccessCode.ROOM_IMAGE_UPLOAD_
 
 @Service
 @RequiredArgsConstructor
-public class RoomImageServiceImpl {
+public class RoomImageServiceImpl implements RoomImageService {
 
     private final RoomImageRepository roomImageRepository;
+    private final S3UploaderService s3UploaderService;
 
     /**
-     * 매물 이미지 업로드를 위한 서비스 메서드
+     * 매물 이미지를 로컬에 업로드하기 위한 서비스 메서드
      * @param roomImage MultipartFile 이미지 파일
      * @param room 이미지를 업로드할 매물
      * @return Success Response
      * @throws AppException 매물 파일 저장 실패 시 AppException(IMAGE_UPLOAD_FAILED) 발생
      */
+    @Override
     @Transactional
     public SuccessResponse<NoneResponse> uploadImage(MultipartFile roomImage, Room room) throws AppException {
         validateImageFile(roomImage);
@@ -58,6 +63,38 @@ public class RoomImageServiceImpl {
         return new SuccessResponse<>(ROOM_IMAGE_UPLOAD_SUCCESS, NoneResponse.NONE);
     }
 
+    /**
+     * 매물 이미지를 S3에 업로드하기 위한 서비스 메서드
+     * @param roomImage MultipartFile 이미지 파일
+     * @param room 이미지를 업로드할 매물
+     * @return Success Response
+     * @throws AppException 매물 파일 저장 실패 시 AppException(IMAGE_UPLOAD_FAILED) 발생
+     */
+    @Override
+    @Transactional
+    public SuccessResponse<NoneResponse> uploadImageToS3(MultipartFile roomImage, Room room) throws AppException {
+        validateImageFile(roomImage);
+        //파일명: UUID + 사진 원래이름
+        String fileName = UUID.randomUUID().toString().replace("-", "") + "_" + roomImage.getOriginalFilename();
+
+        //실제 저장 경로
+        String dbFilePath = "/upload/roomImages/" + fileName;
+        Path filePath = Paths.get("src/main/resources/static", dbFilePath);
+        List<RoomImage> roomImageList = room.getRoomImages();
+
+        try {
+            if (!Files.exists(filePath)) {
+                Files.createDirectories(filePath.getParent());
+            }
+            Files.write(filePath, roomImage.getBytes());
+            roomImageList.add(new RoomImage(room,s3UploaderService.uploadToS3(filePath.toFile(),"roomImages")));
+        } catch (IOException e) {
+            throw new AppException(IMAGE_UPLOAD_FAILED);
+        }
+
+        return new SuccessResponse<>(ROOM_IMAGE_UPLOAD_SUCCESS, NoneResponse.NONE);
+    }
+
 
     /**
      * 매물 이미지 삭제 서비스 메서드
@@ -65,6 +102,7 @@ public class RoomImageServiceImpl {
      * @return SuccessResponse
      * @throws AppException 매물 이미지 삭제 실패 시 IMAGE_DELETE_FAILED 발생
      */
+    @Override
     @Transactional
     public SuccessResponse<NoneResponse> deleteImage(Long roomImageId) throws AppException {
         RoomImage roomImage = roomImageRepository.findById(roomImageId)
