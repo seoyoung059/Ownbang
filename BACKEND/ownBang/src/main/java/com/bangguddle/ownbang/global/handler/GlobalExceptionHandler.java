0a7b8113d2dd360e9265a22a29e9af0c798e48b6
서정희 <sjhlko@naver.com>
2024-08-01 01:30:39 +0900

    ♻️ Refactor: ErrorCode를 static import로 수정

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/handler/GlobalExceptionHandler.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/handler/GlobalExceptionHandler.java
@@ -16,13 +16,15 @@ import org.springframework.web.method.annotation.HandlerMethodValidationExceptio
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import static com.bangguddle.ownbang.global.enums.ErrorCode.*;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // BAD_CREDENTIAL handler
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<Object> handleAuthenticationException() {
-        return makeErrorResponse(ErrorCode.BAD_CREDENTIAL);
+        return makeErrorResponse(BAD_CREDENTIAL);
     }
 
     // custom error handler
@@ -35,7 +37,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleNoResourceFoundException(
             NoResourceFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        return makeErrorResponse(ErrorCode.NOT_FOUND);
+        return makeErrorResponse(NOT_FOUND);
     }
 
     // METHOD_NOT_ALLOWED error(405) handler
@@ -43,7 +45,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
             HttpRequestMethodNotSupportedException ex, HttpHeaders headers,
             HttpStatusCode status, WebRequest request) {
-        return makeErrorResponse(ErrorCode.METHOD_NOT_ALLOWED);
+        return makeErrorResponse(METHOD_NOT_ALLOWED);
     }
 
     // BAD_REQUEST error(400) handler
@@ -51,7 +53,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex, HttpHeaders headers,
             HttpStatusCode status, WebRequest request) {
-        return makeErrorResponse(ErrorCode.BAD_REQUEST);
+        return makeErrorResponse(BAD_REQUEST);
     }
 
     // BAD_REQUEST error(400) handler
@@ -59,13 +61,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleHandlerMethodValidationException(
             HandlerMethodValidationException ex, HttpHeaders headers,
             HttpStatusCode status, WebRequest request) {
-        return makeErrorResponse(ErrorCode.BAD_REQUEST);
+        return makeErrorResponse(BAD_REQUEST);
     }
 
     // INTERNAL_SERVER_ERROR error(500) handler
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> commonExceptionHandler() {
-        return makeErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
+        return makeErrorResponse(INTERNAL_SERVER_ERROR);
     }
 
     private ResponseEntity<Object> makeErrorResponse(ErrorCode errorCode) {
