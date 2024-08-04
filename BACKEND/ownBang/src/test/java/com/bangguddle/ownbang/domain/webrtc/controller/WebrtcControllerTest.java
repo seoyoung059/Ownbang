4c1de7f0c9b01c3447115cc176d06d84f9601762
wnso.kim <wnso.kim@gmail.com>
2024-08-04 11:07:47 +0900

    ✅ Test: WebRTC controller 중개인 토큰 삭제 테스트 - #S11P12A702-176 - #S11P12A702-177 - #S11P12A702-178

--- BACKEND/ownBang/src/test/java/com/bangguddle/ownbang/domain/webrtc/controller/WebrtcControllerTest.java
+++ BACKEND/ownBang/src/test/java/com/bangguddle/ownbang/domain/webrtc/controller/WebrtcControllerTest.java
@@ -5,6 +5,7 @@ import com.bangguddle.ownbang.domain.webrtc.dto.WebrtcRemoveTokenRequest;
 import com.bangguddle.ownbang.domain.webrtc.dto.WebrtcTokenResponse;
 import com.bangguddle.ownbang.domain.webrtc.service.impl.WebrtcAgentService;
 import com.bangguddle.ownbang.domain.webrtc.service.impl.WebrtcUserService;
+import com.bangguddle.ownbang.global.enums.NoneResponse;
 import com.bangguddle.ownbang.global.handler.AppException;
 import com.bangguddle.ownbang.global.response.Response;
 import com.bangguddle.ownbang.global.response.SuccessResponse;
@@ -50,7 +51,7 @@ public class WebrtcControllerTest {
                 .build();
     }
 
-    private WebrtcRemoveTokenRequest makeCreateRequest(Long reservationId, String token){
+    private WebrtcRemoveTokenRequest makeRemoveRequest(Long reservationId, String token){
         return WebrtcRemoveTokenRequest.builder()
                 .reservationId(reservationId)
                 .token(token)
@@ -297,5 +298,99 @@ public class WebrtcControllerTest {
         // verify
         verify(webrtcAgentService, never()).getToken(any(), any());
         verify(webrtcUserService, atLeast(1)).getToken(any(), any());
+    }
+
+    @Test
+    @DisplayName("중개인 토큰 삭제 성공")
+    void 중개인_토큰_삭제_성공() throws Exception {
+        // given
+        WebrtcRemoveTokenRequest removeRequest = makeRemoveRequest(10L, token);
+        SuccessResponse success =
+                new SuccessResponse<>(REMOVE_TOKEN_SUCCESS, NoneResponse.NONE);
+
+        // when
+        when(grantedAuthority.contains(new SimpleGrantedAuthority("ROLE_AGENT")))
+                .thenReturn(true);
+        when(webrtcAgentService.removeToken(removeRequest, userId))
+                .thenReturn(success);
+
+        // then
+        assertThatCode(() ->webrtcController.removeToken(removeRequest, userId, authentication))
+                .doesNotThrowAnyException();
+        assertThat(webrtcController.removeToken(removeRequest, userId, authentication).getStatusCode())
+                .isEqualTo(REMOVE_TOKEN_SUCCESS.getHttpStatus());
+        assertThat(webrtcController.removeToken(removeRequest, userId, authentication))
+                .isInstanceOf(ResponseEntity.class)
+                .isEqualTo(Response.success(success));
+
+        // verify
+        verify(webrtcAgentService, atLeast(1)).removeToken(any(), any());
+        verify(webrtcUserService, never()).removeToken(any(), any());
+    }
+
+    @Test
+    @DisplayName("중개인 토큰 삭제 실패 - 유효하지 않은 예약")
+    void 중개인_토큰_삭제_실패__유효하지_않은_예약() throws Exception {
+        // given
+        WebrtcRemoveTokenRequest removeRequest = makeRemoveRequest(10L, token);
+
+        // when
+        when(grantedAuthority.contains(new SimpleGrantedAuthority("ROLE_AGENT")))
+                .thenReturn(true);
+        when(webrtcAgentService.removeToken(removeRequest, userId))
+                .thenThrow(new AppException(RESERVATION_NOT_FOUND));
+
+        // then
+        assertThatThrownBy(() -> webrtcController.removeToken(removeRequest, userId, authentication))
+                .isInstanceOf(AppException.class)
+                .hasFieldOrPropertyWithValue("errorCode", RESERVATION_NOT_FOUND);
+
+        // verify
+        verify(webrtcAgentService, atLeast(1)).removeToken(any(), any());
+        verify(webrtcUserService, never()).removeToken(any(), any());
+    }
+
+    @Test
+    @DisplayName("중개인 토큰 삭제 실패 - 확정된 예약이 아닌 경우")
+    void 중개인_토큰_삭제_실패__확정된_예약이_아닌_경우() throws Exception {
+        // given
+        WebrtcRemoveTokenRequest removeRequest = makeRemoveRequest(10L, token);
+
+        // when
+        when(grantedAuthority.contains(new SimpleGrantedAuthority("ROLE_AGENT")))
+                .thenReturn(true);
+        when(webrtcAgentService.removeToken(removeRequest, userId))
+                .thenThrow(new AppException(RESERVATION_STATUS_NOT_CONFIRMED));
+
+        // then
+        assertThatThrownBy(() -> webrtcController.removeToken(removeRequest, userId, authentication))
+                .isInstanceOf(AppException.class)
+                .hasFieldOrPropertyWithValue("errorCode", RESERVATION_STATUS_NOT_CONFIRMED);
+
+        // verify
+        verify(webrtcAgentService, atLeast(1)).removeToken(any(), any());
+        verify(webrtcUserService, never()).removeToken(any(), any());
+    }
+
+    @Test
+    @DisplayName("중개인 토큰 삭제 실패 - 이미 삭제된 세션")
+    void 중개인_토큰_삭제_실패__이미_삭제된_세션() throws Exception {
+        // given
+        WebrtcRemoveTokenRequest removeRequest = makeRemoveRequest(10L, token);
+
+        // when
+        when(grantedAuthority.contains(new SimpleGrantedAuthority("ROLE_AGENT")))
+                .thenReturn(true);
+        when(webrtcAgentService.removeToken(removeRequest, userId))
+                .thenThrow(new AppException(BAD_REQUEST));
+
+        // then
+        assertThatThrownBy(() -> webrtcController.removeToken(removeRequest, userId, authentication))
+                .isInstanceOf(AppException.class)
+                .hasFieldOrPropertyWithValue("errorCode", BAD_REQUEST);
+
+        // verify
+        verify(webrtcAgentService, atLeast(1)).removeToken(any(), any());
+        verify(webrtcUserService, never()).removeToken(any(), any());
     }
 }
