f5d7beb31ecf937635b3f331566d21cf31232c7f
wnso.kim <wnso.kim@gmail.com>
2024-08-05 02:20:14 +0900

    🐛 Fix: VideoRepository @Repository 추가

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/video/repository/VideoRepository.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/video/repository/VideoRepository.java
@@ -2,9 +2,11 @@ package com.bangguddle.ownbang.domain.video.repository;
 
 import com.bangguddle.ownbang.domain.video.entity.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
     Optional<Video> findByReservationId(Long reservationId);
 }
