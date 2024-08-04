c337fb71b76bb5135e9961bf93f3f1aeba6a25b4
wnso.kim <wnso.kim@gmail.com>
2024-08-05 00:28:33 +0900

    ♻️ Refactor: Video Service, javaDoc 작성 및 validate 메소드화 - #S11P12A702-313

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/video/service/impl/VideoServiceImpl.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/video/service/impl/VideoServiceImpl.java
@@ -26,11 +26,16 @@ public class VideoServiceImpl implements VideoService {
     private ReservationRepository reservationRepository;
     private VideoRepository videoRepository;
 
+    /**
+     * 녹화된 영상을 조회하는 메소드.<br/>
+     * 영상 상태가 RECORDED 인 경우만 정상 반환
+     * @param videoId
+     * @return VideoSearchResponse
+     */
     @Override
-    public SuccessResponse<VideoSearchResponse> getVideo(Long videoId) {
-        Video video = videoRepository.findById(videoId)
-                .orElseThrow(() -> new AppException(BAD_REQUEST));
-
+    public SuccessResponse<VideoSearchResponse> getVideo(final Long videoId) {
+        // 영상 유효성 검사
+        Video video = validateVideo(videoId);
         if(video.getVideoStatus() == VideoStatus.RECORDING){
             throw new AppException(VIDEO_IS_BEING_RECORDED);
         }
@@ -38,15 +43,28 @@ public class VideoServiceImpl implements VideoService {
         return new SuccessResponse<>(VIDEO_FIND_SUCCESS, VideoSearchResponse.from(video));
     }
 
+    /**
+     * 녹화 영상을 등록하는 메소드.<br/>
+     * WebRTC Service에서 호출됨.
+     * @param request
+     * @return NoneResponse
+     */
     @Override
-    public SuccessResponse<NoneResponse> registerVideo(VideoRecordRequest request) {
+    public SuccessResponse<NoneResponse> registerVideo(final VideoRecordRequest request) {
         // 예약 유효성 검사
         Long reservationId = request.reservationId();
         Reservation reservation = validateReservation(reservationId);
 
-        // 기존 녹화 상태 유효성 검사 - private 추출
-        videoRepository.findByReservationId(reservationId)
-                .ifPresent(i -> {throw new AppException(VIDEO_DUPLICATE);});
+        // 기존 녹화 유효성 검사
+        validateVideoByReservation(reservationId);
+        
+        // request 유효성 검사
+        if(request.videoUrl() == null
+                || request.videoUrl().equals("")
+                || request.videoStatus() != VideoStatus.RECORDING
+        ){
+            throw new AppException(BAD_REQUEST);
+        }
 
         // 녹화 상태 저장
         Video video = request.toEntity(reservation);
@@ -56,12 +74,17 @@ public class VideoServiceImpl implements VideoService {
         return new SuccessResponse<>(VIDEO_RECORD_SUCCESS,NoneResponse.NONE);
     }
 
+    /**
+     * 녹화 영상의 URL과 상태를 수정하는 메소드 <br/>
+     * 기존 상태가 RECORDING 인 경우만 수정 가능
+     * @param request
+     * @param videoId
+     * @return NoneResponse
+     */
     @Override
-    public SuccessResponse<NoneResponse> modifyVideo(VideoUpdateRequest request, Long videoId) {
+    public SuccessResponse<NoneResponse> modifyVideo(final VideoUpdateRequest request, final Long videoId) {
         // 영상 유효성 검사
-        Video video = videoRepository.findById(videoId)
-                .orElseThrow(() -> new AppException(BAD_REQUEST));
-
+        Video video = validateVideo(videoId);
         if(video.getVideoStatus() != VideoStatus.RECORDING){
             throw new AppException(VIDEO_DUPLICATE);
         }
@@ -81,7 +104,8 @@ public class VideoServiceImpl implements VideoService {
         return new SuccessResponse<>(VIDEO_UPDATE_SUCCESS,NoneResponse.NONE);
     }
 
-    private Reservation validateReservation(Long reservationId){
+    // 해당 예약이 유효한지 확인
+    private Reservation validateReservation(final Long reservationId){
         // 예약 repo로 접근해 Reservation 을 얻어와 확인
         Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(
                 () -> new AppException(RESERVATION_NOT_FOUND)
@@ -92,5 +116,17 @@ public class VideoServiceImpl implements VideoService {
         }
 
         return reservation;
+    }
+
+    // 해당 예약과 연결된 녹화 영상 유무 확인
+    private void validateVideoByReservation(final Long reservationId){
+        videoRepository.findByReservationId(reservationId)
+                .ifPresent(i -> {throw new AppException(VIDEO_DUPLICATE);});
+    }
+
+    // 녹화 영상 유무 확인
+    private Video validateVideo(final Long videoId){
+        return videoRepository.findById(videoId)
+                .orElseThrow(() -> new AppException(BAD_REQUEST));
     }
 }
