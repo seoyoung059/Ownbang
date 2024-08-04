18f27a56365e9bc5b1626dfe96aa088704bdea0e
wnso.kim <wnso.kim@gmail.com>
2024-08-04 18:35:49 +0900

    ✨ Feat: Video DTO 작성 - #S11P12A702-313
    - Video 단건 저장 - VideoRecordRequest
    - Video 단건 조회 - VideoSearchResponse
    - Video 단건 수정 - VideoUpdateRequest

---
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/video/dto/VideoUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.bangguddle.ownbang.domain.video.dto;
+
+import com.bangguddle.ownbang.domain.reservation.entity.Reservation;
+import com.bangguddle.ownbang.domain.video.entity.Video;
+import com.bangguddle.ownbang.domain.video.entity.VideoStatus;
+import lombok.Builder;
+
+@Builder
+public record VideoUpdateRequest(Long reservationId, String videoUrl, VideoStatus videoStatus) {
+
+    public Video toEntity(Reservation reservation){
+        return Video.builder()
+                .reservation(reservation)
+                .videoUrl(this.videoUrl)
+                .videoStatus(this.videoStatus)
+                .build();
+    }
+}
