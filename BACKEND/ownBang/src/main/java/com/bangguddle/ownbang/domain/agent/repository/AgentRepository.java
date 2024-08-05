2bbaa2a0198d302e5eb2727f97cad68025e3a529
김서영 <popolley@gmail.com>
2024-08-05 14:47:48 +0900

    ♻️ Refactor: AgentRepository의 getByUserId validation 추가

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/agent/repository/AgentRepository.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/agent/repository/AgentRepository.java
@@ -1,6 +1,8 @@
 package com.bangguddle.ownbang.domain.agent.repository;
 
 import com.bangguddle.ownbang.domain.agent.entity.Agent;
+import com.bangguddle.ownbang.global.enums.ErrorCode;
+import com.bangguddle.ownbang.global.handler.AppException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface AgentRepository extends JpaRepository<Agent, Long> {
     boolean existsByLicenseNumber(String licenseNumber);
 
-    Agent getByUserId(Long userId);
+    default Agent getByUserId(Long userId) {
+        return findById(userId).orElseThrow(()->new AppException(ErrorCode.ACCESS_DENIED));
+    };
 }
