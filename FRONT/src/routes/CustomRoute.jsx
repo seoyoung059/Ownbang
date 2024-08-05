0b494f355bddec543c607892f43266fd2b00248b
iltae94 <iltae94@gmail.com>
2024-08-05 15:41:51 +0900

    :sparkles: Feat: 로그인 상태에 따른 페이지 이동 상세화 #S11P12A702-386

---
+++ FRONT/src/routes/CustomRoute.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useBoundStore } from "../store/store";
+
+export const AgentRoute = ({ element }) => {
+  const { isAuthenticated, user } = useBoundStore((state) => ({
+    isAuthenticated: state.isAuthenticated,
+    user: state.user,
+  }));
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      toast.info("로그인이 필요합니다.", {
+        position: "bottom-left",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    } else if (user && !user.isAgent) {
+      toast.info("중개인만 이용할 수 있습니다.", {
+        position: "bottom-left",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  }, [isAuthenticated, user]);
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  if (user && !user.isAgent) {
+    return <Navigate to="/" />;
+  }
+
+  return <>{element}</>;
+};
+
+export const UserRoute = ({ element }) => {
+  const isAuthenticated = useBoundStore((state) => state.isAuthenticated);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      toast.info("로그인이 필요합니다.", {
+        position: "bottom-left",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  }, [isAuthenticated]);
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return <>{element}</>;
+};
