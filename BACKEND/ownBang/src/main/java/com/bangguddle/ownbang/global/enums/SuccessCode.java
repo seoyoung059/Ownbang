722cbbf5008090ad27f4625da5e58b4b66ce8c5d
서정희 <sjhlko@naver.com>
2024-08-06 00:14:44 +0900

    ✨ Feat: 로그아웃 관련 success code 추가

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/enums/SuccessCode.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/enums/SuccessCode.java
@@ -13,6 +13,7 @@ public enum SuccessCode implements ResponseCode {
     CHECK_PHONE_NUMBER_DUPLICATE_SUCCESS(HttpStatus.OK, "전화번호 중복 조회가 성공적으로 완료되었습니다."),
     LOGIN_SUCCESS(HttpStatus.CREATED, "로그인이 성공적으로 완료되었습니다."),
     PASSWORD_CHECK_SUCCESS(HttpStatus.CREATED, "비밀번호 확인이 성공적으로 완료되었습니다."),
+    LOGOUT_SUCCESS(HttpStatus.CREATED, "로그아웃이 성공적으로 완료되었습니다."),
 
     // Agent Auth API
     UPGRADE_SUCCESS(HttpStatus.CREATED, "중개인 계정으로 성공적으로 전환되었습니다."),
