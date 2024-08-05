97aa26b5edcf2f83f67649dd9190d87d2d5b1262
서정희 <sjhlko@naver.com>
2024-08-05 10:11:38 +0900

    ✨ Feat: 중개인 회원가입 기능 추가

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/config/security/SecurityConfig.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/config/security/SecurityConfig.java
@@ -27,11 +27,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     private static final String[] REQUIRE_USER_ARRAY = {
             /* 임차인 권한 필요 URL */
-            "/rooms", "/bookmarks"
+            "/rooms", "/bookmarks",
+            "/agents/auths/**"
     };
     private static final String[] REQUIRE_AGENT_ARRAY = {
             /* 중개인 권한 필요 URL */
-            "/rooms/agents/**"
+            "/rooms/agents/**",
     };
     private final UserDetailsService userDetailsService;
     private final JwtTokenFilter jwtTokenFilter;
