77120b0f8e4ffa3ac4deeeca35babecc834237e1
wnso.kim <wnso.kim@gmail.com>
2024-08-05 13:14:06 +0900

    ♻️ Refactor: Video DTO valid 추가 - #S11P12A702-313

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/video/dto/VideoRecordRequest.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/video/dto/VideoRecordRequest.java
@@ -3,10 +3,23 @@ package com.bangguddle.ownbang.domain.video.dto;
 import com.bangguddle.ownbang.domain.reservation.entity.Reservation;
 import com.bangguddle.ownbang.domain.video.entity.Video;
 import com.bangguddle.ownbang.domain.video.entity.VideoStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 
 @Builder
-public record VideoRecordRequest(Long reservationId, String videoUrl, VideoStatus videoStatus) {
+public record VideoRecordRequest(
+        @NotNull
+        @Positive
+        Long reservationId,
+
+        @NotBlank
+        String videoUrl,
+
+        @NotNull
+        VideoStatus videoStatus
+) {
 
     public Video toEntity(Reservation reservation){
         return Video.builder()
--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/video/dto/VideoUpdateRequest.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/video/dto/VideoUpdateRequest.java
@@ -3,7 +3,16 @@ package com.bangguddle.ownbang.domain.video.dto;
 import com.bangguddle.ownbang.domain.reservation.entity.Reservation;
 import com.bangguddle.ownbang.domain.video.entity.Video;
 import com.bangguddle.ownbang.domain.video.entity.VideoStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 
 @Builder
-public record VideoUpdateRequest(String videoUrl, VideoStatus videoStatus) { }
+public record VideoUpdateRequest(
+        @NotBlank
+        String videoUrl,
+
+        @NotNull
+        VideoStatus videoStatus
+) { }
