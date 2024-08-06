395a7116353e4510aee896b80f1d547e8bc1d2f7
김서영 <popolley@gmail.com>
2024-08-06 09:49:38 +0900

    ♻️ Refactor: 불필요한 부분 삭제

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/service/RoomImageService.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/service/RoomImageService.java
@@ -2,13 +2,12 @@ package com.bangguddle.ownbang.domain.room.service;
 
 import com.bangguddle.ownbang.domain.room.entity.Room;
 import com.bangguddle.ownbang.global.enums.NoneResponse;
-import com.bangguddle.ownbang.global.handler.AppException;
 import com.bangguddle.ownbang.global.response.SuccessResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface RoomImageService {
 
-    SuccessResponse<NoneResponse> uploadImageToS3(MultipartFile roomImage, Room room) throws AppException;
+    SuccessResponse<NoneResponse> uploadImageToS3(MultipartFile roomImage, Room room);
 
-    SuccessResponse<NoneResponse> deleteImage(Long roomId, Long roomImageId) throws AppException;
+    SuccessResponse<NoneResponse> deleteImage(Long roomId, Long roomImageId);
 }
