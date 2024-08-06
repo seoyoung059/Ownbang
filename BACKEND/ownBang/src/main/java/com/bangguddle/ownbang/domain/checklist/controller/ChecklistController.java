c77b45cbf48abcd939b6c353ad0e71bc725d7e17
wnso.kim <wnso.kim@gmail.com>
2024-08-06 11:06:34 +0900

    ♻️ Refactor: Checklist Controller @Valid 추가 및 javadoc 작성 - #S11P12A702-165

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/checklist/controller/ChecklistController.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/checklist/controller/ChecklistController.java
@@ -19,14 +19,28 @@ public class ChecklistController {
 
     private final ChecklistService checklistService;
 
+    /**
+     * 제목과 항목을 받아 체크리스트 템플릿을 생성합니다. <br/>
+     * @param userId
+     * @param request 제목, 항목
+     * @return NoneResponse
+     */
     @PostMapping("/template")
     public ResponseEntity<Response<NoneResponse>> registerChecklistTemplate(
             @AuthenticationPrincipal Long userId,
-            @RequestBody ChecklistTemplateCreateRequest request) {
+            @RequestBody @Valid ChecklistTemplateCreateRequest request) {
         SuccessResponse<NoneResponse> response = checklistService.registerChecklistTemplate(userId, request);
         return Response.success(response);
     }
 
+
+    /**
+     * 예약 번호, 제목, 항목을 받아 체크리스트 객체를 생성합니다.<br/>
+     * 동일한 예약 번호라면, 제목과 항목이 덮어쓰기 됩니다.
+     * @param userId
+     * @param request 예약번호, 제목, 항목
+     * @return NoneResponse
+     */
     @PostMapping("/non-template")
     public ResponseEntity<Response<NoneResponse>> registerChecklist(
             @AuthenticationPrincipal Long userId,
@@ -35,6 +49,15 @@ public class ChecklistController {
         return Response.success(response);
     }
 
+
+    /**
+     * checklistId와 동일한 체크리스트를 반환합니다.<br/>
+     * 체크리스트 템플릿 또는 객체를 반환할 수 있습니다.<br/>
+     * 마이페이지에서 템플릿 수정 또는 다시보기에서 객체 확인용으로 사용됩니다.
+     * @param userId
+     * @param checklistId 체크리스트 아이디
+     * @return ChecklistSearchResponse
+     */
     @GetMapping("/{checklistId}")
     public ResponseEntity<Response<ChecklistSearchResponse>> getChecklist(
             @AuthenticationPrincipal Long userId,
@@ -43,6 +66,13 @@ public class ChecklistController {
         return Response.success(response);
     }
 
+
+    /**
+     * 체크리스트 템플릿 목록을 반환합니다.<br/>
+     * 마이페이지 템플릿 목록 조회에서 사용합니다.
+     * @param userId
+     * @return ChecklistSearchAllResponse
+     */
     @GetMapping
     public ResponseEntity<Response<ChecklistSearchAllResponse>> getChecklistTemplates(
             @AuthenticationPrincipal Long userId) {
@@ -50,6 +80,15 @@ public class ChecklistController {
         return Response.success(response);
     }
 
+
+    /**
+     * checklistId와 동일한 체크리스트 템플릿을 수정합니다.<br/>
+     * request의 내용으로 제목과 항목을 수정합니다.
+     * @param userId
+     * @param checklistId
+     * @param request 제목, 항목
+     * @return NoneResponse
+     */
     @PatchMapping("/{checklistId}")
     public ResponseEntity<Response<NoneResponse>> modifyChecklistTemplate(
             @AuthenticationPrincipal Long userId,
@@ -59,6 +98,13 @@ public class ChecklistController {
         return Response.success(response);
     }
 
+
+    /**
+     * checklistId와 동일한 체크리스트 템플릿을 삭제합니다.
+     * @param userId
+     * @param checklistId
+     * @return NoneResponse
+     */
     @DeleteMapping("/{checklistId}")
     public ResponseEntity<Response<NoneResponse>> removeChecklistTemplate(
             @AuthenticationPrincipal Long userId,
