b0f677b4dede5e771b1734954558ae04863a017a
서정희 <sjhlko@naver.com>
2024-08-07 10:34:57 +0900

    ✨ Feat: 비밀번호 변경 관련 Success code 추가

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/enums/SuccessCode.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/enums/SuccessCode.java
@@ -14,6 +14,7 @@ public enum SuccessCode implements ResponseCode {
     LOGIN_SUCCESS(HttpStatus.CREATED, "로그인이 성공적으로 완료되었습니다."),
     PASSWORD_CHECK_SUCCESS(HttpStatus.CREATED, "비밀번호 확인이 성공적으로 완료되었습니다."),
     LOGOUT_SUCCESS(HttpStatus.CREATED, "로그아웃이 성공적으로 완료되었습니다."),
+    PASSWORD_CHANGE_SUCCESS(HttpStatus.CREATED, "비밀번호 변경이 성공적으로 완료되었습니다."),
 
     // Agent Auth API
     UPGRADE_SUCCESS(HttpStatus.CREATED, "중개인 계정으로 성공적으로 전환되었습니다."),
