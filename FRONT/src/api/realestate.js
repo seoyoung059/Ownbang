c6fd3639a50da9c34740b2588bef6540b0165079
dlthgml1501 <dlthgml1501@gmail.com>
2024-08-07 16:40:49 +0900

    :sparkles: Feat: 매물 전체 조회 API 수정

--- FRONT/src/api/realestate.js
+++ FRONT/src/api/realestate.js
@@ -2,7 +2,7 @@ import Axios from "./index";
 
 // 매물 전체 정보 확인 API 요청
 export const getRealEstateAll = async () => {
-  const response = await Axios.get(`/rooms/`);
+  const response = await Axios.get(`/rooms/search`);
   return response.data;
 };
 // 매물 상세 정보 확인 API 요청
