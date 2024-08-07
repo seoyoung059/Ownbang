cfe2cb96d8f962dfc1bb71e07aa8a25e9a4b729a
서정희 <sjhlko@naver.com>
2024-08-07 12:42:08 +0900

    ✨ Feat: 로그 생성 전역 처리

---
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/global/config/security/filter/LoggingFilter.java
@@ -0,0 +1,27 @@
+package com.bangguddle.ownbang.global.config.security.filter;
+
+import com.bangguddle.ownbang.global.utils.LogFormatUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain chain)
+            throws ServletException, IOException {
+        chain.doFilter(request, response);
+        log.info(LogFormatUtils.getHttpLog(request, response));
+    }
+}
