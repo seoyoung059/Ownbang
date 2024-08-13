ccf64d3a4e47e3ee5d0f5fc0d8eab30b9e997712
김준영 <wnso.kim@gmail.com>
2024-08-13 15:02:22 +0900

    Revert "Merge branch 'back/room-search-fix' into 'back/master'"
    This reverts merge request !174

--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/controller/RoomController.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/controller/RoomController.java
@@ -93,9 +93,8 @@ public class RoomController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<Response<List<RoomInfoSearchResponse>>> getRooms(@AuthenticationPrincipal Long userId, @RequestParam(name = "lat")
-                                                                           Float lat, @RequestParam(name = "lon") Float lon ) {
-        SuccessResponse<List<RoomInfoSearchResponse>> response = roomService.search(userId, lat, lon);
+    public ResponseEntity<Response<List<RoomInfoSearchResponse>>> getRooms(@AuthenticationPrincipal Long userId) {
+        SuccessResponse<List<RoomInfoSearchResponse>> response = roomService.search(userId);
         return Response.success(response);
     }
 }
--- BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/repository/RoomRepository.java
+++ BACKEND/ownBang/src/main/java/com/bangguddle/ownbang/domain/room/repository/RoomRepository.java
@@ -16,13 +16,11 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
     List<Room> getByAgentId(Long agentId, Pageable pageable);
     List<Room> findAll();
 
-    List<Room> findByGeoHashStartsWith(String geoHashPrefix);
-  /**
+    /**
      * 해당 ID의 매물이 존재하는지 확인
      * @param roomId 유효성 검사를 수행할 RoomId
      */
     default Room getById(final Long roomId) {
         return findById(roomId).orElseThrow(() -> new AppException(ROOM_NOT_FOUND));
     }
-
 }
